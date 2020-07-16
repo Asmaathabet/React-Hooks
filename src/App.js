9c700770ec718c2e8b8dc8e087512b3b453acd09
Asmaathabet <asmathabet2009@hotmail.com>
2020-07-16 23:37:41 +0300

    useState with previous state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 // import ClassCounter from './components/ClassCounter';
-import HooksCounter from './components/HooksCounter';
+// import HooksCounter from './components/HooksCounter';
+import HooksCounterTwo from './components/HooksCounterTwo';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <HooksCounter />
+      {/* <HooksCounter /> */}
+      <HooksCounterTwo />
     </div>
   );
 }
