08c6045343111099adb69c98a2740e15c4ca6494
Asmaathabet <asmathabet2009@hotmail.com>
2020-07-17 00:47:34 +0300

    useState with Object

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 // import ClassCounter from './components/ClassCounter';
-import ClassCounterTwo from './components/ClassCounterTwo';
+// import ClassCounterTwo from './components/ClassCounterTwo';
 // import HooksCounter from './components/HooksCounter';
 // import HooksCounterTwo from './components/HooksCounterTwo';
+import HooksCounterThree from './components/HooksCounterThree';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <ClassCounterTwo />
+      {/* <ClassCounterTwo /> */}
       {/* <HooksCounter /> */}
       {/* <HooksCounterTwo /> */}
+      <HooksCounterThree />
     </div>
   );
 }
